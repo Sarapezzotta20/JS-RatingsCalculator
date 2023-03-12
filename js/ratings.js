fb2364c132db15a9841856650098d5afcb021861
Sara Pezzotta <sarapezzotta20@hotmail.it>
2023-03-12 20:37:56 +1100

    spelling fix

--- js/ratings.js
+++ js/ratings.js
@@ -4,10 +4,10 @@ function collect_ratings() {
     sum: 0,
     average: 0,
   };
-  let rating = 0;
-  const elements = document.querySelectorAll(".rating");
 
-  elements.foreach((element) => {
+  let rating = 0;
+  const elements = document.querySelectorAll('.rating');
+  elements.forEach((element) => {
     rating = parseInt(element.id.replace("star", ""));
     ratings.count += parseInt(element.value);
     ratings.sum += rating * parseInt(element.value);

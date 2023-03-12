1e09d29b1ea4b68e2329fe3834136397f7e9e155
Sara Pezzotta <sarapezzotta20@hotmail.it>
2023-03-12 20:30:30 +1100

    final

--- js/ratings.js
+++ js/ratings.js
@@ -1,11 +1,25 @@
-
 function collect_ratings() {
-let ratings = {
+  let ratings = {
     count: 0,
     sum: 0,
     average: 0,
-};
-let rating = 0;
-let element = document.querySelectorAll('rating');
+  };
+  let rating = 0;
+  const elements = document.querySelectorAll("rating");
 
+  elements.foreach((element) => {
+    rating = parseInt(element.id.replace("star", ""));
+    ratings.count += parseInt(element.value);
+    ratings.sum += rating * parseInt(element.value);
+  });
+
+  if (ratings.count !== 0) {
+    ratings.average = sum / count;
+  }
+  return collect_ratings(ratings);
 }
+
+document.addEventListener('change', () => {
+    ratings = collect_ratings();
+    ratings.average.value.querySelectorAll(element.id('average'));
+});
